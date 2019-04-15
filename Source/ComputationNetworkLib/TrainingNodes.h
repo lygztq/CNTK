c69385c7b881092f46ecd8142c51d27fba8e7469
Tianqi Zhang <v-tizhan@microsoft.com>
2019-04-15 20:47:54 +0800

    change statstype for BN

--- Source/ComputationNetworkLib/TrainingNodes.h
+++ Source/ComputationNetworkLib/TrainingNodes.h
@@ -3789,7 +3789,8 @@ class BatchNormalizationNode : public ComputationNodeNonLooping<ElemType>, publi
         return L"BatchNormalization";
     }
 
-    typedef typename std::conditional<std::is_same<ElemType, half>::value, float, ElemType>::type StatType;
+    //typedef typename std::conditional<std::is_same<ElemType, half>::value, float, ElemType>::type StatType;
+	typedef typename ElemType StatType;
 
     // inputs
     // TODO: Change all of these throughout the codebase to 'class enum'. Also change all places where we still use integer constants.
